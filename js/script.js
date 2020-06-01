684601a9255f4b9a763c23382521cf5013773ddc
Joshua McDonald <mcdojj12@gmail.com>
2020-06-01 19:33:02 +1000

    finished base functionality

--- js/script.js
+++ js/script.js
@@ -3,69 +3,68 @@ var turnFlip = {
     'x': 'o',
     'o': 'x',
 } // this is pretty cheeky. Magic number-ish. I guess cyclers arent that bad though
+var gameOver = false;
 
-var playArray = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+var xMoves = [];
+var oMoves = [];
 
-/**
- * takes an x or y position and converts its into a 0,1, or 2 based on which third it lies in.
- * @param {num} position 
- */
-const getMoveFromPos = function (val) {
-    if (val < 33) {
-        return (0);
-    } else if ((val >= 33) && (val < 66)) {
-        return (1);
-    } else if (val >= 66) {
-        return (2);
-    }
-}
-
-const refreshBoard = function () {
-    for (let i = 0; i < playArray.length; i++) {
-        let row = [];
-        for (let j = 0; j < playArray.length; j++) {
-            // console.log(playArray[i][j]);
-            let entry = playArray[i][j];
-            if(entry){
-                console.log("value here", entry);
-                var $img = $(`<img src="./img/${turn}.png">`);
-                $img.addClass(turn);
-                $img.css("left", event.pageX);
-                $img.css("top", event.pageY);
-                $img.appendTo('.content');
-            }
-            row.push(playArray[i][j]);
+const checkWinCondition = function (moveArray) {
+    const combos = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ];
+    for (let comboIndex = 0; comboIndex < combos.length; comboIndex++) {
+        let combo = combos[comboIndex];
+        let condition = combo.every((e) => moveArray.includes(e));
+        if (condition) {
+            console.log("game over");
+            return (true);
         }
-
     }
 }
+
+
 
 $(document).ready(function () {
-    var mousePositionRelativeToBoard = {};
-    $('#board').on('click', function (event) {
-        // translate the currently clicked position into which square was clicked. 
-        // need to translate global mosue pos to relative to the image. 
-        let move = {}
-        console.log("image clicked");
-        var offset = $(this).offset();
-        mousePositionRelativeToBoard.x = 100 * (event.pageX - offset.left) / this.width;
-        mousePositionRelativeToBoard.y = 100 * (event.pageY - offset.top) / this.width;
-        // console.log(mousePositionRelativeToBoard);
-        move.x = getMoveFromPos(mousePositionRelativeToBoard.x);
-        move.y = getMoveFromPos(mousePositionRelativeToBoard.y);
-        if (!playArray[move.y][move.x]) {
-            playArray[move.y][move.x] = turn;
-            // console.log(playArray);
-            turn = turnFlip[turn];
-        } // else ignore click on already used position
-        refreshBoard();
-        // Dont worry about positioning images yet. 
-        // #############################################################
-       
-        // #############################################################
+    $('img').on('click', function (event) {
+        if (xMoves.length+oMoves.length ==9){
+            gameOver=true;
+            alert("Cats Game.");
+        }
+        if (!gameOver) {
+            if (!xMoves.includes(this.id) && !oMoves.includes(this.id)) { // if square empty
+                turn == 'x' ? xMoves.push(parseInt(this.id)) : oMoves.push(parseInt(this.id));
+                $(this).attr("src", `./img/${turn}.png`);
+                turn = turnFlip[turn];
+            } // else ignore click on already used position
+            if (checkWinCondition(xMoves)) {
+                console.log("x wins");
+                alert("x's Game.");
+
+                gameOver = true;
+            } else if (checkWinCondition(oMoves)) {
+                alert("o's Game.");
+                console.log("o wins");
+                gameOver = true;
+
+            }
+        }
     });
+
+    $("#reset").on('click', function () {
+        // reset all images to whiteSquare
+        $('img').attr('src', './img/whiteSquare.png');
+        // reset Arrays
+        xMoves = [];
+        oMoves = [];
+        gameOver = false;
+
+    })
+
 });
